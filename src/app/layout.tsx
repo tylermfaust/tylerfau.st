e7d98b8884f89d01a03561d90db0776cb1a4b9c1
Tyler Faust <tyler.m.faust@gmail.com>
2024-04-16 18:25:19 -0700

    change initial copyright date

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
         </nav>
         {children}
         <footer className="absolute bottom-4 flex h-12 w-full justify-between px-4 py-8 font-bold text-[#545454]">
-          © 2021-{year} Tyler Faust
+          © 2020-{year} Tyler Faust
         </footer>
       </body>
     </html>
